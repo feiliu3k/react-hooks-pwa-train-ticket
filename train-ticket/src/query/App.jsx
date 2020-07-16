3c6c438c1902eed59a96a6730ccfc287172fb1e4
baill <bailonglong@aliyun.com>
2020-07-16 13:38:40 +0800

    useEffect query.html参数解析, 拉取数据

--- train-ticket/src/query/App.jsx
+++ train-ticket/src/query/App.jsx
@@ -1,14 +1,40 @@
-import React, { useState, useEffect } from 'react'
-
+import React, { useState, useEffect, useCallback } from 'react'
 import { connect } from 'react-redux'
+import dayjs from 'dayjs'
+import { h0 } from '../common/fp'
+import URI from 'urijs'
+import Header from '../common/Header'
 import Nav from '../common/Nav'
 import List from './List'
 import Bottom from './Bottom'
 import './style/App.css'
+import { setFrom, setTo, setDepartDate, setHighSpeed, setSearchParsed } from './actions'
 function App(props) {
+	const { from, to, departDate, highSpeed, dispatch, searchParsed, orderType, onlyTickets, checkedTicketTypes, checkedTrainTypes, checkedDepartStations, checkedArriveStations, departTimeStart, departTimeEnd, arriveTimeStart, arriveTimeEnd } = props
+	useEffect(() => {
+		const queries = URI.parseQuery(window.location.search)
+		const { from, to, date, highSpeed } = queries
+		dispatch(setFrom(from))
+		dispatch(setTo(to))
+		dispatch(setDepartDate(h0(dayjs(date).valueOf())))
+		dispatch(setHighSpeed(highSpeed === 'true' ? true : false))
+		dispatch(setSearchParsed(true))
+	}, [])
+	useEffect(() => {
+		if (!searchParsed) {
+			return
+		}
+		// TODO: fetch
+	}, [from, to, departDate, highSpeed, searchParsed, orderType, onlyTickets, checkedTicketTypes, checkedTrainTypes, checkedDepartStations, checkedArriveStations, departTimeStart, departTimeEnd, arriveTimeStart, arriveTimeEnd])
+	const onBack = useCallback(() => {
+		window.history.back()
+	}, [])
+
 	return (
 		<div>
-			1111
+			<div className='header-wrapper'>
+				<Header title={`${from} -> ${to}`} onBack={onBack} />
+			</div>
 			<Nav></Nav>
 			<List />
 			<Bottom />
